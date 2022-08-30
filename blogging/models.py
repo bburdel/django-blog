47703bb828fc4ee84f05c1e1e517e83d2b4c3cf5
bburdel <94432814+bburdel@users.noreply.github.com>
2022-08-29 19:52:28 -0700

    Beginning Part 2 of the blogging activity.

--- blogging/models.py
+++ blogging/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    """
+    This class defines Post, which maintains the details of a blog post.
+    """
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -12,3 +15,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    """
+    THis class defines Category, which maintains the details of the category of a blog post.
+    """
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+
+
