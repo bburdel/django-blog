19acb1e088351054540af2f9783f5b228925e725
bburdel <94432814+bburdel@users.noreply.github.com>
2022-09-15 20:56:35 -0700

    Code style enforcement using Black

--- blogging/admin.py
+++ blogging/admin.py
@@ -3,40 +3,30 @@ from blogging.models import Post, Category
 
 
 class CategoryInline(admin.TabularInline):
-    """
+    """ """
 
-    """
     model = Category.posts.through
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    """
+    """ """
 
-    """
-    # list_display = ('title', 'author', 'created_date',
-    #                 'modified_date', 'published_date', 'categories')
-    list_display = ('title', 'author', 'created_date',
-                    'modified_date', 'published_date')
+    list_display = (
+        "title",
+        "author",
+        "created_date",
+        "modified_date",
+        "published_date",
+    )
 
     inlines = [
         CategoryInline,
     ]
 
-    # def categories(self, title):
-    #     p = Category.object.posts.all()
-    #     category_dictionary = p.__dict__
-    #     print(category_dictionary)
-    #     for key, value in category_dictionary.items():
-    #         if title == Post.title:
-    #             cat_name = value
-    #     assert isinstance(cat_name, object)
-    #     return cat_name
-
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    """
+    """ """
 
-    """
-    exclude = ('posts',)
+    exclude = ("posts",)
