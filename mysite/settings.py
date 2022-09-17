44599f2c4de03eb52097bec31243b3d671fe386e
bburdel <94432814+bburdel@users.noreply.github.com>
2022-09-17 13:17:23 -0700

    Added allauth changes to settings and updated urls to include the path for allauth.urls
    This is a commit before migrating per the instructions here: https://learndjango.com/tutorials/django-allauth-tutorial

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
+
     "polling",
     "blogging",
 ]
@@ -121,4 +128,17 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+
+
+# allauth backend configuration
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_VERIFICATION = "none"
+
 LOGIN_REDIRECT_URL = "/"
